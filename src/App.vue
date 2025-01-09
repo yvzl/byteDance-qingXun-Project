2e506d5281edd574f88431165cc7eccd30a764c6
大大 <1322168192@qq.com>
2025-01-09 23:17:37 +0800

    大大test

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import test from './components/test.vue';
+</script>
+
+<template>
+  <div class="list">
+    <test msg="大大"  />
+
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1em;
+  
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <div>this is {{msg}}</div>
+</template>
+<script setup lang="ts">
+//import { ref } from 'vue';
+defineProps({
+    msg: String
+});
+</script>
