c1af982d453c272eb5d644febb94f3b959b52e28
yvzl <898101408@qq.com>
2025-01-21 16:41:26 +0800

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import {createRouter, createWebHistory, type RouteRecordRaw} from "vue-router"
 
+// 配置 vue-router
+
 const routes: RouteRecordRaw[] = [{
     path: "/",
-    component: () => import("@/views/home.vue")
+    component: () => import("@/views/Home.vue")
 }]
 
 export default createRouter({
