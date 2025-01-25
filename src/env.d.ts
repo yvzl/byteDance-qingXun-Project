80013220d6f0305c094135047b2f85f875015492
yvzl <898101408@qq.com>
2025-01-26 00:01:37 +0800

    移除冗余文件

--- src/env.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.vue' {
-    import type { DefineComponent } from 'vue'
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-    const component: DefineComponent<{}, {}, any>
-    export default component
-  }
-//typescript 只能理解 .ts 文件，无法理解 .vue文件。因此需要给.vue文件加上类型说明文件  
