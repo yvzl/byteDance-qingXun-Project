1b3c02e1ecd810de093bcc6bd02df2a7767f5c62
yvzl <898101408@qq.com>
2025-01-26 00:09:41 +0800

    完成 chat 头像组件

---
+++ src/components/Head.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ src: string }>()
+</script>
+
+<template>
+  <div class="head">
+    <img :src="src" alt="head"/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/styles/Head.module";
+</style>
