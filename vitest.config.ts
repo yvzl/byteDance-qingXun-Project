affcd5c76f8a8268c377fff2901b69cf84a2f4ff
yvzl <898101408@qq.com>
2025-01-26 00:04:04 +0800

    格式化文件

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vitest/config';
+import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'node', // 使用 Node.js 环境
-    setupFiles: ['./vite.config.ts'], // 如果你有全局设置文件
-    deps: {
-      inline: ['utils'], // 确保 utils 模块被内联
+    test: {
+        globals: true,
+        environment: 'node', // 使用 Node.js 环境
+        setupFiles: ['./vite.config.ts'], // 如果你有全局设置文件
+        deps: {
+            inline: ['utils'], // 确保 utils 模块被内联
+        },
     },
-  },
 });
