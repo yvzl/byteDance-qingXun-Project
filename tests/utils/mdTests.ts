b3120c4ae36984011f8bf790d3e805479c548621
yvzl <898101408@qq.com>
2025-01-26 00:14:39 +0800

    优化测试用例

--- tests/utils/mdTests.ts
+++ tests/utils/mdTests.ts
@@ -1,10 +1,5 @@
 import {md} from "src/utils"
 
 // 测试 markdown 转 html
-
-console.log(md.render(`\`\`\`js
-const a = 1
-\`\`\`
-`))
-
-console.log("# aaa")
+console.log(md.render(`\`\`\`js\nconst a = 1\`\`\``))
+console.log(`# aaa\n## aaa`)
