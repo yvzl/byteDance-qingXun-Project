8f77a6d6e83bdf7007b09b1ac996ee5ff5b2ce8d
yvzl <898101408@qq.com>
2025-01-26 00:12:21 +0800

    完成 user 对话组件

---
+++ src/components/MessageUser.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import MessageItem from '@/components/MessageItem.vue'
+
+defineProps<{value: string}>()
+</script>
+
+<template>
+  <div class="message-user">
+    <MessageItem :value="value" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/styles/MessageUser.module";
+</style>
