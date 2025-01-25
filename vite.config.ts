7d5340424c28bccc714bc6eb1453dd732f59fbc0
yvzl <898101408@qq.com>
2025-01-26 00:04:33 +0800

    格式化文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import {resolve} from 'path';
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src'),
+        },
     },
-  },
 })
