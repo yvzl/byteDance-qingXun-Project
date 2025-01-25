fef79bcc1042046aee7e7c27cee4495a1a4ca6f0
yvzl <898101408@qq.com>
2025-01-26 00:16:32 +0800

    新增内容组件

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {computed} from "vue";
+import {messageStore} from "@/stores"
+import {storeToRefs} from "pinia"
+import MessageList from "@/components/MessageList.vue";
+import InputBox from "@/components/InputBox.vue";
+
+const store = messageStore()
+const {data, messageId} = storeToRefs(store)
+
+const messageData = computed(() => data.value.filter(({id}) => id === messageId.value)[0].content)
+</script>
+
+<template>
+  <div class="main">
+    <MessageList :data="messageData"/>
+    <InputBox/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/styles/Main.module";
+</style>
